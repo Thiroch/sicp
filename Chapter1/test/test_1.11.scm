74f9504db4c52f5c787f2e9f33c39ae71098c0e9
Thiroch <161222629+Thiroch@users.noreply.github.com>
2025-06-06 00:58:42 +0900

    Refactor assertions in test_1.11.scm to include descriptive names for better clarity in test results

--- Chapter1/test/test_1.11.scm
+++ Chapter1/test/test_1.11.scm
@@ -1,39 +1,42 @@
 #lang sicp
 (#%require "../src/1.11.scm")
 
-(define (assert expected actual)
-  (= expected actual))
+(define (assert name expected actual)
+  (display name)
+  (if (= expected actual)
+      (display "Pass\n")
+      (display "Fail\n")))
 
 ; n < 3
-(assert 0 (f-recursive 0))
-(assert 1 (f-recursive 1))
-(assert 2 (f-recursive 2))
+(assert "f-recursive n=0: " 0 (f-recursive 0))
+(assert "f-recursive n=1: " 1 (f-recursive 1))
+(assert "f-recursive n=2: " 2 (f-recursive 2))
 
 ; n = 3
-(assert 4 (f-recursive 3))
+(assert "f-recursive n=3: " 4 (f-recursive 3))
 
 ; n > 3
-(assert 11 (f-recursive 4))
-(assert 25 (f-recursive 5))
-(assert 59 (f-recursive 6))
-(assert 142 (f-recursive 7))
-(assert 335 (f-recursive 8))
-(assert 796 (f-recursive 9))
-(assert 1892 (f-recursive 10))
+(assert "f-recursive n=4: " 11 (f-recursive 4))
+(assert "f-recursive n=5: " 25 (f-recursive 5))
+(assert "f-recursive n=6: " 59 (f-recursive 6))
+(assert "f-recursive n=7: " 142 (f-recursive 7))
+(assert "f-recursive n=8: " 335 (f-recursive 8))
+(assert "f-recursive n=9: " 796 (f-recursive 9))
+(assert "f-recursive n=10: " 1892 (f-recursive 10))
 
 ; n < 3
-(assert 0 (f-iterative 0))
-(assert 1 (f-iterative 1))
-(assert 2 (f-iterative 2))
+(assert "f-iterative n=0: " 0 (f-iterative 0))
+(assert "f-iterative n=1: " 1 (f-iterative 1))
+(assert "f-iterative n=2: " 2 (f-iterative 2))
 
 ; n = 3
-(assert 4 (f-iterative 3))
+(assert "f-iterative n=3: " 4 (f-iterative 3))
 
 ; n > 3
-(assert 11 (f-iterative 4))
-(assert 25 (f-iterative 5))
-(assert 59 (f-iterative 6))
-(assert 142 (f-iterative 7))
-(assert 335 (f-iterative 8))
-(assert 796 (f-iterative 9))
-(assert 1892 (f-iterative 10))
+(assert "f-iterative n=4: " 11 (f-iterative 4))
+(assert "f-iterative n=5: " 25 (f-iterative 5))
+(assert "f-iterative n=6: " 59 (f-iterative 6))
+(assert "f-iterative n=7: " 142 (f-iterative 7))
+(assert "f-iterative n=8: " 335 (f-iterative 8))
+(assert "f-iterative n=9: " 796 (f-iterative 9))
+(assert "f-iterative n=10: " 1892 (f-iterative 10))
