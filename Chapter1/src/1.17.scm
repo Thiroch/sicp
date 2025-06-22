96dfb0d65ff47e48a73571418ac763413b29dc89
Thiroch <161222629+Thiroch@users.noreply.github.com>
2025-06-22 16:20:53 +0900

    Add solution for Exercise 1.17

---
+++ Chapter1/src/1.17.scm
@@ -0,0 +1,11 @@
+#lang sicp
+(#%provide fast-*)
+(#%require racket/trace)
+
+(define (double n) (* n 2))
+(define (halve n) (/ n 2))
+
+(define (fast-* a b)
+    (cond ((= b 0) 0)
+          ((even? b) (fast-* (double a) (halve b)))
+          (else (+ a (fast-* a (- b 1))))))
